3673daedbf81c2db20d8a1c1ea56d23e7e77275b
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2024-11-01 19:35:31 +0330

    chore: remove commented codes

--- apps/transactions/serializers.py
+++ apps/transactions/serializers.py
@@ -26,15 +26,3 @@ class ChargeCustomerSerializer(serializers.ModelSerializer):
         model = Sell
         fields = ['id', 'seller', 'telephone', 'amount', 'timestamp']
         read_only_fields = ['timestamp']
-
-
-# class ChargeCustomerSerializer(serializers.Serializer):
-#     seller = serializers.SlugRelatedField(queryset=SellerProfile.objects.select_related("user"),
-#                                           slug_field='user__username')
-#     customer_id = serializers.IntegerField(required=True)
-#     amount = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=5000, max_value=100000)
-#
-#     def validate_amount(self, value):
-#         if value <= 0:
-#             raise serializers.ValidationError("Amount must be greater than zero.")
-#         return value
