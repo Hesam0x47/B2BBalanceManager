6fe0c86d6c6266f6907fc94f87eda794a0bd5f5b
Hessam Ghassemi <s.hesam.ghasemi@gmail.com>
2024-11-02 02:33:03 +0330

    refactor: get 'REDIS_ADDR' from env

--- b2b_balance_manager/settings.py
+++ b2b_balance_manager/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Update with your Redis instance
+        'LOCATION': f'redis://{os.getenv("REDIS_ADDR")}:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
